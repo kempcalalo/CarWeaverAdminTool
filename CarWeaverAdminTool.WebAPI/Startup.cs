894ca1e95e949a279ce117653318264b5fccd63d
kcalalo <kemp.calalo@volvocars.com>
2018-11-08 15:22:55 +0100

    added dependency resolver

--- CarWeaverAdminTool.WebAPI/Startup.cs
+++ CarWeaverAdminTool.WebAPI/Startup.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using Ninject;
 
 [assembly: OwinStartup(typeof(CarWeaverAdminTool.WebAPI.Startup))]
 namespace CarWeaverAdminTool.WebAPI
